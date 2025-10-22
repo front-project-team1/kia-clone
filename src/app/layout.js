a32edd3a0deb5e0df7911e36f417acee99edc0a4
shin-minhyuk <afs_style@naver.com>
2025-10-22 09:57:10 +0900

    [feat] route groups 사용 형태로 수정 // (auth) 에서는 rootLayout 안보여지도록 수정.

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Header from "@/components/common/Header";
-import Footer from "@/components/common/Footer";
 import AuthProviderWrapper from "@/components/providers/AuthProviderWrapper";
 
 const geistSans = Geist({
@@ -29,11 +27,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <AuthProviderWrapper>
-          <Header />
-          {children}
-          <Footer />
-        </AuthProviderWrapper>
+        <AuthProviderWrapper>{children}</AuthProviderWrapper>
       </body>
     </html>
   );
